a9d6b916cc54acc0877dd97f02057f34ec4d941d
kumanan <kuamanankmk17gmail.com>
2025-07-03 14:01:20 +0100

    firebase  added

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey:process.env.NEXT_PUBLIC_API,
   authDomain:process.env.NEXT_PUBLIC_AUTH_DOMAIN ,
@@ -13,5 +11,11 @@ const firebaseConfig = {
   appId:process.env.NEXT_PUBLIC_API_ID 
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = ()=>signInWithPopup(auth, provider); 
